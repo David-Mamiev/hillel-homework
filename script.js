67d33412d68642fd58a2772ce23f66c3a01a67c6
David <davidmamiev1592@gmail.com>
2020-11-16 18:32:44 +0200

    push files

---
+++ script.js
@@ -0,0 +1,13 @@
+let numberPolz1 = Number(prompt("Последовательно введите несколько числовых значений"));
+let numberPolz2 = Number(prompt("Последовательно введите несколько числовых значений"));
+let numberPolz3 = Number(prompt("Последовательно введите несколько числовых значений"));
+let numberPolz4 = Number(prompt("Последовательно введите несколько числовых значений"));
+console.log((numberPolz1 + numberPolz2 + numberPolz3 + numberPolz4) / 4);
+
+
+/*let arr = [Number(prompt("Введите числа"))];
+console.log(arr);
+
+let birth = Number(prompt("Введите ваш год рождения (от 1920 до 2020)"));
+let age = 2020 - birth;
+console.log("Вам " + age + " лет");*/
