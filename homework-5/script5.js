a341ee4d930e26938daaee46e60f5b8b5bbd4b6d
David <davidmamiev1592@gmail.com>
2020-11-28 15:38:55 +0200

    homework 5

--- homework-5/script5.js
+++ homework-5/script5.js
@@ -1,4 +1,4 @@
-/*let number = Number(prompt("Введите число"));
+let number = Number(prompt("Введите число"));
 let degree = Number(prompt("Введите степень, в которую хотите возвести число"));
 const exponentiation = function (number, degree) {
     let raisedNumber = 1;
@@ -8,39 +8,22 @@ const exponentiation = function (number, degree) {
     console.log(raisedNumber);
 };
 exponentiation (number, degree);
-exponentiation (2, 3);*/
 
 
 
 let yourNumb;
 let rand = Math.floor(Math.random() * (10 - 1) + 1);
-
-/*while (!(Number(yourNumb) === rand || Number(yourNumb) === null)) {
-    yourNumb = prompt("Попробуйте угадать число (от 0 до 10)");
-    console.log("Ваше число: " + yourNumb);
-    if (yourNumb === null){
-        console.log("Вы вышли из игры");
-        break;
-    }
-    if (Number(yourNumb) === rand) {
-        console.log("Вы угадали!");
-        break;
-    }
-}*/
 const zapusk = function (yourNumb) {
-    yourNumb = prompt("Попробуйте угадать число (от 0 до 10)");
-    console.log("Ваше число: " + yourNumb);
-    while (!(Number(yourNumb) === rand || Number(yourNumb) === null)) {
+    while (!(Number(yourNumb) === rand || yourNumb === null)) {
         yourNumb = prompt("Попробуйте угадать число (от 0 до 10)");
         console.log("Ваше число: " + yourNumb);
-        if (yourNumb === null){
-            console.log("Вы вышли из игры");
-            break;
-        }
-        proverka (yourNumb, rand);
+        proverka(yourNumb, rand);
     }
 };
 const proverka = function (yourNumb, rand) {
+    if (yourNumb === null){
+        console.log("Вы вышли из игры");
+    }
     if (Number(yourNumb) === rand) {
         console.log("Вы угадали!");
     } 
